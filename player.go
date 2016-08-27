8823972bd25a8fa567ac5bb1bf4483e4440a7e1b
32hiko <nemurenaimachi@gmail.com>
2016-08-27 14:27:04 +0900

    手だけ評価する方式で行く。

--- player.go
+++ player.go
@@ -23,26 +23,46 @@ func (player *Player) search() (bestmove string, score int) {
 		return
 	}
 	// TODO 定跡があればそこから指す
-	// 指す前の評価値
+
+	// depth 2で読むのがこのブロック。
+	base_score := 0
+	base_sfen := ban.toSFEN(true)
 	teban := ban.teban
-	is_oute := ban.isOute(teban)
-	if is_oute {
-		score = 0
-	} else {
-		score = evaluate(ban, teban)
-	}
-	usiResponse("info string null_move_score: " + fmt.Sprint(score))
+	score = 0
 	index := -1
-	// TODO 普通に探索する
-	master_sfen := ban.toSFEN(true)
 	// TODO 1手指して戻す、を高速に実現できるようにする。
 	for i, move := range moves.moves_map {
-		new_score := doSearch(master_sfen, move, teban, 1)
-		if new_score > score {
-			score = new_score
-			index = i
+		next_ban := newBanFromSFEN(base_sfen)
+		next_ban.applySFENMove(move.toUSIMove())
+		next_ban.komap = newKomap(next_ban)
+		if next_ban.isOute(teban) {
+			// ここでの王手は自殺手を意味する。評価できない。
+			continue
+		}
+		// 評価値テーブルがあるなら、ここで参照する
+		my_move_score := evaluateMove(ban, move)
+		if my_move_score < base_score {
+			// 必要なら評価値を保存
+			// 悪くなる手は読まない
+			continue
+		}
+		next_moves := generateAllMoves(next_ban)
+		for _, next_move := range next_moves.moves_map {
+			enemy_move_score := evaluateMove(next_ban, next_move)
+			if my_move_score-enemy_move_score > base_score {
+				// いい手として扱う
+				// 例 [76歩, 84歩]
+				if score < my_move_score {
+					score = my_move_score
+					index = i
+					usiResponse("info string: " + fmt.Sprint(score) + " move: " + moves.moves_map[index].toUSIMove())
+				}
+			}
 		}
 	}
+	// いい手順だけ返してくる
+	// いい手順を再びbanに適用し、そこからdepth２で読ませる、というのを繰り返す。playerの設定でdepthを決めておく
+
 	// TODO 時間配分
 	// TODO 送信
 	if index == -1 {
@@ -52,6 +72,11 @@ func (player *Player) search() (bestmove string, score int) {
 		return
 	}
 	bestmove = moves.moves_map[index].toUSIMove()
+	return
+}
+
+func evaluateMove(ban *Ban, move *Move) (score int) {
+	score = 0
 	return
 }
 
