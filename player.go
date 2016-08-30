321c88590c87522ec1ca1a2f7330f12d45204d8a
32hiko <nemurenaimachi@gmail.com>
2016-08-30 23:04:01 +0900

    1手詰み搭載のランダム気味プレーヤー。割打ちの銀、ふんどしの桂など、両取りをかけることができる。

--- player.go
+++ player.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type PlayerConfig struct {
 	btime   int
 	wtime   int
@@ -25,10 +23,10 @@ func (player *Player) search() (bestmove string, score int) {
 	// TODO 定跡があればそこから指す
 
 	// depth 2で読むのがこのブロック。
-	base_score := -999
+	my_move_base_score := -9999
 	base_sfen := ban.toSFEN(true)
 	teban := ban.teban
-	score = -999
+	score = -9999
 	index := -1
 	// TODO 1手指して戻す、を高速に実現できるようにする。
 	for i, move := range moves.moves_map {
@@ -41,22 +39,34 @@ func (player *Player) search() (bestmove string, score int) {
 		}
 		// 評価値テーブルがあるなら、ここで参照する
 		my_move_score := evaluateMove(next_ban, move)
-		if my_move_score < base_score {
+		if my_move_score < my_move_base_score {
 			// 必要なら評価値を保存
-			// 悪くなる手は読まない
+			// 極端に悪くなる手は読まない
 			continue
 		}
 		next_moves := generateAllMoves(next_ban)
+		if next_moves.count() == 0 {
+			// 相手の手がないのは詰み。
+			score = 9999
+			bestmove = moves.moves_map[i].toUSIMove()
+			return
+		}
+		enemy_move_best_score := -9999
 		for _, next_move := range next_moves.moves_map {
 			enemy_move_score := evaluateMove(next_ban, next_move)
-			if my_move_score-enemy_move_score > base_score {
-				// いい手として扱う
-				// 例 [76歩, 84歩]
-				if score < my_move_score {
-					score = my_move_score
-					index = i
-					usiResponse("info string: " + fmt.Sprint(score) + " move: " + moves.moves_map[index].toUSIMove())
-				}
+			if enemy_move_score > enemy_move_best_score {
+				enemy_move_best_score = enemy_move_score
+			}
+		}
+		if enemy_move_best_score == -9999 {
+			// 相手のいい手がない。
+			score = 9999
+			bestmove = moves.moves_map[i].toUSIMove()
+			return
+		} else {
+			if (my_move_score - enemy_move_best_score) > score {
+				score = my_move_score - enemy_move_best_score
+				index = i
 			}
 		}
 	}
@@ -99,7 +109,7 @@ func evaluateMove(ban *Ban, move *Move) (score int) {
 	reverse_kiki := ban.komap.getTebanReverseKiki(teban)
 	// 今の手の利きの数を加算する
 	kiki_masu := reverse_kiki.kiki_map[move.to]
-	score += reverse_kiki.count(move.to)
+	// score += reverse_kiki.count(move.to)
 	for _, kiki_to := range kiki_masu {
 		koma, exists := ban.komap.all_koma[kiki_to]
 		if exists {
@@ -109,12 +119,23 @@ func evaluateMove(ban *Ban, move *Move) (score int) {
 			}
 		}
 	}
-	// 相手の利きが多いマスへの手は減点する
+	teban_kiki := ban.getTebanKiki(teban)
 	aite_kiki := ban.getTebanKiki(teban.aite())
-	score -= aite_kiki.count(move.to) * 30
+	// 移動元について
+	// 駒がどいたことによる影響
+	if teban_kiki.count(move.from) > 0 {
+		score += teban_kiki.count(move.from) * 10
+	}
+	// 移動先について
+	// 駒がきたことによる影響
+	// 相手の利きが多いマスへの手は減点する
+	if aite_kiki.count(move.to) > teban_kiki.count(move.to) {
+		score -= int((move.kind + 1) * 100)
+	}
+
 	// 前進する手を評価
 	if move.isForward(teban) {
-		score += 10
+		score += int(NO_KIND-move.kind) * 5
 	}
 
 	return
